f61e0b9a55765c3ecd275a75b74194cd00f160c8
brandon-guerra <brandong481@gmail.com>
2022-12-01 09:21:59 -0600

    file restructure and day 1 2022

--- advent-of-code-2021.playground/Sources/Solve.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-public func solve<T>(partNumber: Int, input: T, function: (T) -> Any) {
-    let start = CFAbsoluteTimeGetCurrent()
-    let solution = function(input)
-    let elapsedTime = CFAbsoluteTimeGetCurrent() - start
-    print("The solution to part \(partNumber) is \(solution)")
-    print("Execution time: \(elapsedTime)s")
-}
