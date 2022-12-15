2f85678da8953d900a61670081d0da5311304606
brandon-guerra <brandong481@gmail.com>
2022-12-15 13:32:56 -0600

    chore: remove comment

--- 2022/Day8/day8.rb
+++ 2022/Day8/day8.rb
@@ -24,8 +24,6 @@ def get_scenic_score (start, rest_in_line)
     return score
 end
 
-# puts get_scenic_score(4, [3,1,1,1,3,2,1,2,3,3,1,2,4,1,2,1,1,4,7,8,9])
-
 scenic_scores = []
 
 visible_tree_count = 0
